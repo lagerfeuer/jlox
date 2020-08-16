291b7ecba48813f35ea86bb43a201854281846b4
Lukas Deutz <lukas.deutz@mailfence.com>
2020-08-16 17:22:01 -0400

    Add class instances

---
+++ src/main/java/at/lagerfeuer/lox/LoxInstance.java
@@ -0,0 +1,14 @@
+package at.lagerfeuer.lox;
+
+public class LoxInstance {
+    final private LoxClass klass;
+
+    LoxInstance(LoxClass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String toString() {
+        return klass.name + " instance";
+    }
+}
